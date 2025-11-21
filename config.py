61261c1ff1efa2f436258e7a0dd6ad3bc7e2b365
Fadhil <muhammadfadhildarussalam.fd@gmail.com>
2025-11-21 17:56:58 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,26 @@
+class Config:
+    BASE_URL = "https://www.saucedemo.com/"
+    
+    # Library credentials for different user scenarios
+    CREDENTIALS = {
+        "valid": {
+            "username": "standard_user",
+            "password": "secret_sauce"
+        },
+        "locked": {
+            "username": "locked_out_user",
+            "password": "secret_sauce"
+        },
+        "invalid_user": {
+            "username": "",
+            "password": "secret_sauce"
+        },
+        "invalid_pass": {
+            "username": "standard_user",
+            "password": ""
+        },
+        "empty_user": {
+            "username": "",
+            "password": "secret_sauce"
+        }
+    }
