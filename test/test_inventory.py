7840227de90f540b640fd5546323a4866f6df115
Fadhil <muhammadfadhildarussalam.fd@gmail.com>
2025-11-23 21:27:25 +0700

    checkpoint9
    fixing screenshot issues on test_inventory, and adding bug reporting on spreadsheets

--- test/test_inventory.py
+++ test/test_inventory.py
@@ -49,80 +49,126 @@ class TestInventory(unittest.TestCase):
         login_pg.login(user['username'], user['password'])
         
         self.wait.until(EC.url_contains("inventory.html"))
+    
+    def _take_screenshot(self, user_key, test_name):
+        if not os.path.exists("screenshots"):
+            os.makedirs("screenshots")
+        
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        
+        filename = f"screenshots/{test_name}_{user_key}_{timestamp}.png"
+        
+        try:
+            self.driver.save_screenshot(filename)
+            print(f"\n Screenshot Error  '{user_key}' Saved IN: {filename}")
+        except Exception as e:
+            print(f"Failed to take screenshot: {e}")
         
     def test_sort_z_to_a(self):
-        """TC 4: Memastikan Sortir Z-A berfungsi untuk semua user"""
-        
+        """SL_TC_009: Sortir Z-A"""
         users_to_test = ['valid', 'problem', 'glitch']
-        
         for user in users_to_test:
             with self.subTest(user=user):
-                self._login_as(user)
-                
-                inventory_pg = InventoryPage(self.driver)
-                inventory_pg.select_sort_option("za")
-                
-                actual_names = inventory_pg.get_all_product_names()
-                expected_names = sorted(actual_names, reverse=True)
-                
-                self.assertEqual(actual_names, expected_names, f"Sorting Z-A gagal pada user: {user}")
+                try:
+                    self._login_as(user)
+                    inventory_pg = InventoryPage(self.driver)
+                    
+                    # Pilih Z to A
+                    inventory_pg.select_sort_option("za")
+                    
+                    actual = inventory_pg.get_all_product_names()
+                    expected = sorted(actual, reverse=True) # Z-A
+                    
+                    self.assertEqual(actual, expected, f"Sorting Z-A gagal pada user: {user}")
+                except Exception as e:
+                    self._take_screenshot(user, "test_sort_z_to_a")
+                    raise e
+
+    def test_sort_a_to_z(self):
+        """SL_TC_010: Sortir A-Z"""
+        users_to_test = ['valid', 'problem', 'glitch']
+        for user in users_to_test:
+            with self.subTest(user=user):
+                try:
+                    self._login_as(user)
+                    inventory_pg = InventoryPage(self.driver)
+                    
+                    # Trik: Pilih Z-A dulu, baru A-Z (biar kelihatan berubah)
+                    inventory_pg.select_sort_option("za")
+                    inventory_pg.select_sort_option("az")
+                    
+                    actual = inventory_pg.get_all_product_names()
+                    expected = sorted(actual) # A-Z (Default)
+                    
+                    self.assertEqual(actual, expected, f"Sorting A-Z gagal pada user: {user}")
+                except Exception as e:
+                    self._take_screenshot(user, "test_sort_a_to_z")
+                    raise e
 
     def test_sort_low_to_high(self):
-        """TC 6: Memastikan Sortir Harga Rendah-Tinggi berfungsi"""
-        
+        """SL_TC_011: Sortir Harga Rendah-Tinggi"""
         users_to_test = ['valid', 'problem', 'glitch']
-        
         for user in users_to_test:
             with self.subTest(user=user):
-                self._login_as(user)
-                
-                inventory_pg = InventoryPage(self.driver)
-                inventory_pg.select_sort_option("lohi")
-                
-                actual_prices = inventory_pg.get_all_product_prices()
-                expected_prices = sorted(actual_prices)
-                
-                self.assertEqual(actual_prices, expected_prices, f"Sorting Lo-Hi gagal pada user: {user}")
+                try:
+                    self._login_as(user)
+                    inventory_pg = InventoryPage(self.driver)
+                    
+                    # Pilih Low to High
+                    inventory_pg.select_sort_option("lohi")
+                    
+                    actual = inventory_pg.get_all_product_prices()
+                    expected = sorted(actual) # Kecil ke Besar
+                    
+                    self.assertEqual(actual, expected, f"Sorting Lo-Hi gagal pada user: {user}")
+                except Exception as e:
+                    self._take_screenshot(user, "test_sort_low_to_high")
+                    raise e
+
+    def test_sort_high_to_low(self):
+        """SL_TC_012: Sortir Harga Tinggi-Rendah"""
+        users_to_test = ['valid', 'problem', 'glitch']
+        for user in users_to_test:
+            with self.subTest(user=user):
+                try:
+                    self._login_as(user)
+                    inventory_pg = InventoryPage(self.driver)
+                    
+                    # Pilih High to Low
+                    inventory_pg.select_sort_option("hilo")
+                    
+                    actual = inventory_pg.get_all_product_prices()
+                    expected = sorted(actual, reverse=True) # Besar ke Kecil
+                    
+                    self.assertEqual(actual, expected, f"Sorting Hi-Lo gagal pada user: {user}")
+                except Exception as e:
+                    self._take_screenshot(user, "test_sort_high_to_low")
+                    raise e
 
     def test_product_images_validity(self):
-        """TC 8: Memastikan gambar produk tampil benar (kecuali problem_user)"""
+        """SL_TC_013: Memastikan gambar produk tampil benar (kecuali problem_user)"""
         
         users_to_test = ['valid', 'problem', 'glitch']
         
         for user in users_to_test:
             with self.subTest(user=user):
-                self._login_as(user)
-                
-                inventory_pg = InventoryPage(self.driver)
-                images = inventory_pg.get_all_images()
-                
-                for img in images:
-                    src = img.get_attribute("src")
+                try:
+                    self._login_as(user)
+                    inventory_pg = InventoryPage(self.driver)
+                    images = inventory_pg.get_all_images()
                     
-                    self.assertTrue(len(src) > 0, "Source gambar ditemukan kosong")
-                    
-                    
-                    if user != 'problem':
-                        self.assertNotIn("sl-404", src, f"Bug! Gambar rusak ditemukan pada user: {user}")
+                    for img in images:
+                        src = img.get_attribute("src")
+                        self.assertTrue(len(src) > 0, "Source kosong")
+                        
+                        if user != 'problem':
+                            self.assertNotIn("sl-404", src, f"Gambar rusak pada {user}")
+                            
+                except Exception as e:
+                    self._take_screenshot(user, "test_images")
+                    raise e
 
     def tearDown(self):
-        if hasattr(self, '_outcome'):
-            result = self._outcome.result
-            if result.errors or result.failures:
-                all_problems = result.errors + result.failures
-                if any(test == self for test, error_msg in all_problems):
-                    if not os.path.exists("screenshots"):
-                        os.makedirs("screenshots")
-                    
-                    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-                    test_name = self._testMethodName
-                    filename = f"screenshots/{test_name}_{timestamp}.png"
-                    
-                    try:
-                        self.driver.save_screenshot(filename)
-                        print(f"\n Screenshot Error Saved IN: {filename}")
-                    except Exception as e:
-                        print(f"Failed to take screenshot: {e}")
         
         self.driver.quit()
 
